2d184efe48afc0e495656438d0fdc96fd830e6fc
Shawn Salat <shawn.salat@gmail.com>
2018-04-19 22:19:44 -0700

    Add Map class at map.rb

---
+++ W1D5/map.rb
@@ -0,0 +1,35 @@
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    if @map.lookup(key)
+      @map.each_index do |i|
+        @map[i][1] = value if @map[i][0] == key
+      end
+    else
+      @map << [key,value]
+    end
+    @map
+  end
+
+  def lookup(key)
+    @map.each do |(k,v)|
+      return v if k == key
+    end
+    nil
+  end
+
+  def remove(key)
+    if @map.lookup(key)
+      @map.reject! {|(k,v)| k == key}
+    else
+      nil
+    end
+  end
+
+  def show
+    @map
+  end
+end
