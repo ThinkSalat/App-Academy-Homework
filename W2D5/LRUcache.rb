0f0f6dd88a4d2e6d7c4ee45361706fa64defbfcf
Shawn Salat <shawn.salat@gmail.com>
2018-04-26 21:20:05 -0700

    implement LRU cache using array

--- W2D5/LRUcache.rb
+++ W2D5/LRUcache.rb
@@ -1,20 +1,31 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @cache = []
+      @size = size
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      @cache.delete(el)
+      @cache << el
+      remove_lru if count > @size
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      @cache
     end
 
     private
     # helper methods go here!
+    attr_reader :cache
 
+    def remove_lru
+      @cache.shift
+    end
   end
