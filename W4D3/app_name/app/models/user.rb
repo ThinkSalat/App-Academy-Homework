56b56303536cc6b3b222059646dadd665ee122f3
Shawn Salat <shawn.salat@gmail.com>
2018-05-08 21:33:26 -0700

    add use auth using salted hash generated through bcrypt

---
+++ W4D3/app_name/app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+
+class User < ApplicationRecord
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  validates :username, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length:{minimum: 8}, allow_nil: true
+
+  def self.find_by_credentials(sign_in,pass_word)
+      user = User.find_by(username: sign_in)
+      if user.is_password?(pass_word)
+        return user
+      else
+        raise 'Invalid credentials'
+      end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.reset_session_token!
+    session_token = self.class.generate_session_token
+    self.save
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+end
