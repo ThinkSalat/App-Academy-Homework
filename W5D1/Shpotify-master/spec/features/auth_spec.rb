58a99c592e63827649ee64fab2d2fb4b6e9bdb09
Shawn Salat <shawn.salat@gmail.com>
2018-05-12 15:51:38 -0700

    finish phase 0 of css homework

--- W5D1/Shpotify-master/spec/features/auth_spec.rb
+++ W5D1/Shpotify-master/spec/features/auth_spec.rb
@@ -2,7 +2,7 @@ feature "the signup process" do
 
   scenario "has a new user page" do
     visit new_user_url
-    expect(page).to have_content "Sign Up!"
+    expect(page).to have_content "Sign Up"
   end
 
   feature "signing up a user" do
@@ -10,7 +10,7 @@ feature "the signup process" do
       visit new_user_url
       fill_in 'email', :with => "testing@email.com"
       fill_in 'password', :with => "biscuits"
-      click_on "Sign Up!"
+      click_on "Sign Up"
     end
 
     scenario "redirects to bands index page after signup"
@@ -20,7 +20,7 @@ feature "the signup process" do
     before(:each) do
       visit new_user_url
       fill_in 'email', :with => "testing@email.com"
-      click_on "Sign Up!"
+      click_on "Sign Up"
     end
 
     scenario "re-renders the signup page after failed signup"
