48cc9b5ab064a8dc5e878740292fb03db0a735b5
Shawn Salat <shawn.salat@gmail.com>
2018-05-22 23:05:58 -0700

    complete ajax homework

--- W6D3/ajax/js/index.js
+++ W6D3/ajax/js/index.js
@@ -1,5 +1,7 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b';
+$.get(url, x=> console.log(x));
 // Add another console log here, outside your AJAX request
+console.log('synced');
