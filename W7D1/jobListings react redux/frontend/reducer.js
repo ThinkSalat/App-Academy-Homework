d15c17d91b1cfa4bff81d3fd841fd3b6e0fd56cf
Shawn Salat <shawn.salat@gmail.com>
2018-05-27 20:49:18 -0700

    remove merge from reducer

--- W7D1/jobListings react redux/frontend/reducer.js
+++ W7D1/jobListings react redux/frontend/reducer.js
@@ -1,27 +1,17 @@
 // jshint esversion: 6
-
-import { merge } from 'lodash';
-
-
-const initialState = {
-  city: "Please Select", 
-  jobs: []
-};
+const initialState = {city: "Please Select", jobs: []};
 
 const reducer = (state = initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case "SWITCH_LOCATION":
-      return merge({},state,{
+      return {
         city: action.city,
         jobs: action.jobs
-      });
-
+      };
     default:
       return state;
   } 
-
-
 };
 
 export default reducer;
